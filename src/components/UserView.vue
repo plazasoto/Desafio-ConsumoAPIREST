3e5d6cca07411ef9677700dfd6d2629f080ebd43
plazasoto <jarrettsguitarsfactory@gmail.com>
2024-09-09 17:06:19 -0300

    datos de API

--- src/components/UserView.vue
+++ src/components/UserView.vue
@@ -1,5 +1,28 @@
 <template>
-    <div>
-        <h2>ᓀ‸ᓂ user ᓀ‸ᓂ</h2>
+    <div v-for="(usuario,i) in usuarios" :key="i">
+        <img :src="usuario.picture.large">
+        <h3>{{usuario.name.first+" "+usuario.name.last}}</h3>
     </div>
 </template>
+
+<script>
+import axios from 'axios';
+
+export default {
+    data(){
+    return {
+      usuarios: []
+    }
+  },
+  methods:{
+    async fetchUser(){
+        const url = "https://randomuser.me/api";
+        const { data } = await axios.get(url);
+        this.usuarios = data.results
+    }
+  },
+  async mounted() {
+    await this.fetchUser();
+  },
+}
+</script>
